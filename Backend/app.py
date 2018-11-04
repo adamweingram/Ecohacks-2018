57d92aa0bc585804f8d801ff5879091be15b46e7
Adam Weingram <adamweingram@gmail.com>
2018-11-04 12:37:06 -0800

    API now uses the database file instead of an in-memory database

--- Backend/app.py
+++ Backend/app.py
@@ -10,21 +10,8 @@ api = Api(app)
 
 class Data(Resource):
     def get(self):
-        con = sqlite3.connect(":memory:")
-        cur = con.cursor()
-        cur.execute(
-            "CREATE TABLE given_data (reading_time text, chw_total text, hw_total text, hw_kl text, chw_kl text, hw_cob text, chw_cob text, hw_SE1 text, chw_SE1 text, hw_SE2 text, chw_SE2 text, hw_SSB text, chw_SSB text, hw_SSM text, chw_SSM text, hw_SAAC text, chw_SAAC text, total_sunpower_kwh text);")
-
-        insert_vars = [str(datetime.datetime.now()), "171.3230092", "201.4196262", "21.8", "42.4", "9.3", "14.7",
-                       "147.5", "82.4", "0", "9.00225", "13.76", "NULL", "NULL", "NULL", "NULL", "NULL", "4863.202475"]
-
-        cur.execute(
-            "INSERT INTO given_data (reading_time, chw_total, hw_total, hw_kl, chw_kl, hw_cob, chw_cob, hw_SE1, chw_SE1, hw_SE2, chw_SE2, hw_SSB, chw_SSB, hw_SSM, chw_SSM, hw_SAAC, chw_SAAC, total_sunpower_kwh) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);",
-            insert_vars)
-
-        cur.execute(
-            "INSERT INTO given_data (reading_time, chw_total, hw_total, hw_kl, chw_kl, hw_cob, chw_cob, hw_SE1, chw_SE1, hw_SE2, chw_SE2, hw_SSB, chw_SSB, hw_SSM, chw_SSM, hw_SAAC, chw_SAAC, total_sunpower_kwh) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);",
-            insert_vars)
+        con = sqlite3.connect("./database.db")
+        # cur = con.cursor()
 
         values = get_rows(con)
 
@@ -32,9 +19,7 @@ class Data(Resource):
         con.close()
 
         data = {
-            'timestamp': str(datetime.datetime.now()),
-            'other': 'foo',
-            'testing': values
+            'data-rows': values[0:20]
         }
 
         return data
